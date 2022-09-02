4d908ba33389f1a3d4b62d03d8f0aa331041dedf
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-09-02 16:20:36 +0300

    add linters

--- enumerable.rb
+++ enumerable.rb
@@ -1,23 +1,23 @@
 module MyEnumerable
-    def all?
-      each do |y|
-        return false unless yield y
-      end
-      true
+  def all?
+    each do |y|
+      return false unless yield y
     end
-  
-    def any?
-      each do |y|
-        return true if yield y
-      end
-      false
-    end
-  
-    def filter
-      new_list = []
-      each do |y|
-        new_list << y if yield y
-      end
-      new_list
-    end
+    true
   end
+
+  def any?
+    each do |y|
+      return true if yield y
+    end
+    false
+  end
+
+  def filter
+    new_list = []
+    each do |y|
+      new_list << y if yield y
+    end
+    new_list
+  end
+end
